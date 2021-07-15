bcccbd87e7ec1aa2771ff15543d65af2e5a86600
Pancham <panchamb63@gmail.com>
2021-07-15 16:37:57 +0530

    Did some minor changes

--- Frontend-trainingLab/sample-app/src/components/navbar icons/navbarIcon.js
+++ Frontend-trainingLab/sample-app/src/components/navbar icons/navbarIcon.js
@@ -60,12 +60,7 @@ function navbarIcon(props) {
                                     <div
                                         className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${props.colorSecA}`}>
                                         <span className="inline-flex justify-center items-center ml-4">
-                                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path strokeLineCap="round" strokeLineJoin="round" strokeWidth="2"
-                                                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
-                                                </path>
-                                            </svg>
+
                                         </span>
                                         <span className="ml-2 text-sm tracking-wide truncate">Courses</span>
                                     </div>
@@ -74,12 +69,7 @@ function navbarIcon(props) {
                                     <div
                                         className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${props.colorSecB}`}>
                                         <span className="inline-flex justify-center items-center ml-4">
-                                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path strokeLineCap="round" strokeLineJoin="round" strokeWidth="2"
-                                                    d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4">
-                                                </path>
-                                            </svg>
+
                                         </span>
                                         <span className="ml-2 text-sm tracking-wide truncate">Test</span>
 
@@ -89,12 +79,7 @@ function navbarIcon(props) {
                                     <div
                                         className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${props.colorSecC}`}>
                                         <span className="inline-flex justify-center items-center ml-4">
-                                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path strokeLineCap="round" strokeLineJoin="round" strokeWidth="2"
-                                                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z">
-                                                </path>
-                                            </svg>
+
                                         </span>
                                         <span className="ml-2 text-sm tracking-wide truncate">Events</span>
                                     </div>
